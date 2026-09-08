311ebe7d920cac0f4a6371710598f189191c7405
DaynMS <160369986+DaynMS@users.noreply.github.com>
2026-09-07 23:28:10 -0500

    Update custom-directives.ts

--- quartz/plugins/transformers/custom-directives.ts
+++ quartz/plugins/transformers/custom-directives.ts
@@ -1,1 +1,23 @@
+import { visit } from "unist-util-visit"
+import { QuartzTransformerPlugin } from "../types"
+import remarkDirective from "remark-directive"
 
+export const CustomDirectives: QuartzTransformerPlugin = () => ({
+  name: "CustomDirectives",
+
+  markdownPlugins() {
+    return [
+      remarkDirective,
+      () => (tree) => {
+        visit(tree, "containerDirective", (node: any) => {
+          node.data = {
+            hName: "div",
+            hProperties: {
+              className: [node.name],
+            },
+          }
+        })
+      },
+    ]
+  },
+})
