4f1bb4677b984b633c0161d98f906affcb4eca22
DaynMS <160369986+DaynMS@users.noreply.github.com>
2026-09-07 23:48:26 -0500

    Update quartz.ts

--- quartz.ts
+++ quartz.ts
@@ -1,6 +1,13 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import { CustomDirectives } from "./quartz/plugins/transformers"
 
-const config = await loadQuartzConfig()
+const config = await loadQuartzConfig({
+  plugins: {
+    transformers: [
+      CustomDirectives(),
+    ],
+  },
+})
 
 export default config
 export const layout = await loadQuartzLayout()
